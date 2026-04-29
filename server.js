27ca76bd00a7f18ede7c70a9a9541354e7709ec7
Revetsllik <57834572+Revetsllik@users.noreply.github.com>
2026-04-30 00:37:38 +0200

    Server implemented. Previous commit holds pre-server state.

---
+++ server.js
@@ -0,0 +1,10 @@
+/*'use strict'
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('.'));
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});*/
